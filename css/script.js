3c9a298bea85c36e3fe131b3c1316a72777e8924
Havish Vivekanandan <63521007+HavishVivek@users.noreply.github.com>
2022-10-07 10:05:44 -0500

    add  navbar js

--- css/script.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector('.navbar')
-
-fetch('https://havishvivek.github.io/havish_vivek/navigation.html')
-.then(res=>res.text())
-.then(data=>(
-  nav.innerHTML = data
-))
